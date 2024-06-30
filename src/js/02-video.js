7e56120a1271d8ab60788ab1489aab6ce62a34b8
silvia80569 <157542289+silvia80569@users.noreply.github.com>
2024-06-30 23:37:12 +0200

    Exercitiul 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+function saveCurrentTime(data) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+const savedTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime).catch(error => {
+    console.error('Failed to set current time:', error);
+  });
+}
